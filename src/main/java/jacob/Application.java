d2ecde4b07d5c0b5df301c67e3bd41ad7e846ead
jkozol <jacobdkozol@gmail.com>
2017-02-20 16:53:25 -0500

    heroku?

--- src/main/java/jacob/Application.java
+++ src/main/java/jacob/Application.java
@@ -2,11 +2,20 @@ package jacob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
