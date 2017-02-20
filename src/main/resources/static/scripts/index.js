859fcd608fbd4da73d170cdc762a3efc27ac2877
jkozol <jacobdkozol@gmail.com>
2017-02-20 16:11:49 -0500

    First Commit

---
+++ src/main/resources/static/scripts/index.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+5 = ||||||||||||||||||||
+4 = ||||||||||||||||
+3 = ||||||||||||
+2 = ||||||||
+1 = ||||
+*/
+
+$(document).ready(function () {
+    $('.table').footable({
+        "columns": $.get('json/columns.json'),
+        "rows": $.get('json/rows.json')
+    });
+});
